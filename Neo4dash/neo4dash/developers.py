d30fc9f628ff60afaa51803c91d64a166a066c9d
Lennard Schaap <lennard.schaap@hotmail.com>
2019-10-29 20:47:10 +0100

    removed developer functions from db

--- Neo4dash/neo4dash/developers.py
+++ Neo4dash/neo4dash/developers.py
@@ -78,3 +78,8 @@ class Developers():
             entry += str(self.dev_last_active(dev))
             print(entry)
             entry = ""
+    # def dev_get_activity(self, dev_id):
+    #     nodes, rels = self.get_all_data(merge = False)
+    #     commits = self.get_rels_type(dev_id, 'Commit')
+    #
+    #     temp = sorted(commits, key = lambda x: x['data'][''])
