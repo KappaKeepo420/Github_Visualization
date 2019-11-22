6790ec563b347545fcaabfc6584f4efe82bb1ef1
tuinlamp <55321100+tuinlamp@users.noreply.github.com>
2019-11-22 12:04:24 +0100

    Add files via upload

---
+++ Neo4dash/neo4dash/files.py
@@ -0,0 +1,18 @@
+from datetime import date
+from singleton import Singleton
+
+class Files(metaclass=Singleton):
+    def __init__(self, nodes, rels):
+        self.nodes = nodes
+        self.rels = rels
+
+    def list_file_name(self):
+        file_nodes = [x for x in self.nodes if x['data']['type'] == 'File']
+        named = []
+
+        for x in file_nodes:
+            named.append(x['data']['label'])
+
+        return named
+
+
